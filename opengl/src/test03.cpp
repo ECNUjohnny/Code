2f7a38506c27abf29154246e828a171628eee6c0
johnny <JohnnyLynd@outlook.com>
2026-04-12 13:21:03 +0800

    修改

--- opengl/src/test03.cpp
+++ opengl/src/test03.cpp
@@ -14,46 +14,144 @@ const int N = 1005;
 using namespace std;
 using namespace glm;
 
-int n, op;
-float xmax = -1e9, xmin = 1e9, ymax = -1e9, ymin = 1e9;
+int n, op, u = 8, d = 4, l = 1, r = 2;
+unsigned int VAO, VBO;
+unsigned int borderVAO, borderVBO;
+char s[N];
+char *edge = "D:/Code/opengl/data/data.txt", *polygon = "D:/Code/opengl/data/data1.txt";
+float xmax = 390, xmin = -390, ymax = 390, ymin = -390, eps = 1e-9;
 struct vertex
 {
     float x, y;
 } v[N];
 
-struct edge
-{
-    vertex st, en;
-} e[N];
-
-glm::mat4 Projection = glm::ortho(-400.0f, 400.0f, -400.0f, 400.0f);
-glm::mat4 V, Current, I;
-ifstream fin;
+glm::mat4 Projection = glm::ortho(-450.0f, 450.0f, -450.0f, 450.0f);
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec2 aPos;\n"
     "uniform mat4 Projection;\n"
-    "uniform mat4 Current;\n"
-    "out vec4 transformPos;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = Projection * Current * vec4(aPos, 0.0, 1.0);\n"
-    "   transformPos = Current * vec4(aPos, 0.0, 1.0);\n"
+    "   gl_Position = Projection * vec4(aPos, 0.0, 1.0);\n"
     "}\n";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-    "in vec4 transformPos;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
     "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
     "}\n";
 
+int code(vertex &v)
+{
+    int code = 0;
+    if (v.x < xmin) code |= l;
+    if (v.x > xmax) code |= r;
+    if (v.y < ymin) code |= d;
+    if (v.y > ymax) code |= u;
+
+    return code;
+}
+
+int clip_edge(vertex &beg, vertex &en)
+{   
+    int code1 = code(beg), code2 = code(en);
+
+    while (1)
+    {
+        if (!(code1 | code2))
+        {
+            return 1;
+        }
+
+        if (code1 & code2)
+        {
+            return 0;
+        }
+
+        vertex &t = code1 ? beg : en;
+        int codeout = code1 ? code1 : code2;
+        float k = fabs(en.x - beg.x) < eps ? 2e15 : (en.y - beg.y) / (en.x - beg.x);
+
+        if (codeout & u)
+        {
+            t.x = t.x + (ymax - t.y) / k;
+            t.y = ymax;
+        }
+        else if (codeout & d)
+        {
+            t.x = t.x + (ymin - t.y) / k;
+            t.y = ymin;
+        }
+        else if (codeout & r)
+        {
+            t.y = t.y + k * (xmax - t.x);
+            t.x = xmax;
+        }
+        else if (codeout & l)
+        {
+            t.y = t.y + k * (xmin - t.x);
+            t.x = xmin;
+        }
+
+        if (codeout == code1) code1 = code(t);
+        else code2 = code(t);
+    }
+
+    return 1;
+}
+
+inline void process_edge(char *loc)
+{
+    FILE *dat = fopen(loc, "r");
+    op = 1;
+    if (!dat)
+    {
+        puts("-1");
+        return;
+    }
+
+    fscanf(dat, "%d", &n);
+    for (int i = 1; i <= n; i++) fscanf(dat, "%f%f", &v[i].x, &v[i].y);
+    fclose(dat);
+
+    //for (int i = 1; i <= n; i++) printf("%f %f\n", v[i].x, v[i].y);
+
+    if (!clip_edge(v[1], v[2])) op = 0;
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, n * sizeof(vertex), &v[1], GL_STATIC_DRAW);
+}
+
+inline void process_polygon(char *loc)
+{
+    FILE *dat = fopen(loc, "r");
+    op = 2;
+    if (!dat)
+    {
+        puts("-1");
+        return;
+    }
+
+    fscanf(dat, "%d", &n);
+    for (int i = 1; i <= n; i++) fscanf(dat, "%f%f", &v[i].x, &v[i].y);
+    fclose(dat);
+}
+
 void CharacterCallBack(GLFWwindow* window, unsigned int codepoint)
 {
     switch(codepoint)
     {
-        
+        case 'e':
+            process_edge(edge);
+            break;
+
+        case 'p':
+            process_polygon(polygon);
+            break;
+
+        default:
+            break;
     }
 }
 
@@ -71,7 +169,6 @@ int main()
 
     glfwSetCharCallback(window, CharacterCallBack);
 
-    unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
@@ -89,7 +186,6 @@ int main()
         -390.0f,  390.0f
     };
 
-    unsigned int borderVAO, borderVBO;
     glGenVertexArrays(1, &borderVAO);
     glGenBuffers(1, &borderVBO);
     glBindVertexArray(borderVAO);
@@ -114,10 +210,8 @@ int main()
     glDeleteShader(frag);
     glDeleteShader(vert);
 
-    int vertexCurrentLocation = glGetUniformLocation(shaderProgram, "Current");
     int vertexProjectLocation = glGetUniformLocation(shaderProgram, "Projection");
     glUseProgram(shaderProgram);
-    glUniformMatrix4fv(vertexCurrentLocation, 1, GL_FALSE, glm::value_ptr(Current));
     glUniformMatrix4fv(vertexProjectLocation, 1, GL_FALSE, glm::value_ptr(Projection));
 
     glPointSize(7.0f);
@@ -129,12 +223,9 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
 
-        glm::mat4 identityMatrix = glm::mat4(1.0f); 
-        glUniformMatrix4fv(vertexCurrentLocation, 1, GL_FALSE, glm::value_ptr(identityMatrix));
         glBindVertexArray(borderVAO);
         glDrawArrays(GL_LINE_LOOP, 0, 4);
 
-        glUniformMatrix4fv(vertexCurrentLocation, 1, GL_FALSE, glm::value_ptr(Current));
         glBindVertexArray(VAO);
         glDrawArrays(GL_LINES, 0, n);
 
