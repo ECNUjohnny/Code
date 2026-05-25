20beadb0faa31267a266e4a98bf138093041af5d
johnny <JohnnyLynd@outlook.com>
2026-05-25 21:29:02 +0800

    segment covering

--- cpp/test01.cpp
+++ cpp/test01.cpp
@@ -13,28 +13,10 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-const int N = 2e5 + 5;
-int t, n, k, a[N], b[N];
+int t;
 
 void solve()
 {
-    scanf("%d%d", &n, &k);  
-    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
-    for (int i = 1; i <= n; i++) scanf("%d", &b[i]);
-
-    ll ans = 0;
-
-    int flg_a = 0, flg_b = 0;
-    for (int i = 1; i < n; i++) if (a[i + 1] < a[i]) {flg_a = 1; break;}
-    for (int i = 1; i < n; i++) if (b[i + 1] < b[i]) {flg_b = 1; break;}
-
-    if (flg_a || flg_b)
-    {
-        for (int i = 1; i <= n; i++) ans += abs(a[i] - b[i]);
-        printf("%lld\n", ans);
-        return;
-    }
-
     
 }
 
@@ -46,4 +28,6 @@ int main()
     {
         solve();
     }
+
+    return 0;
 }
